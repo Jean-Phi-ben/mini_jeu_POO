5f0d6f40ace48c2cae89cabed3d5da0988a9568d
Jean-Phi-ben <benoist_jp@yahoo.de>
2020-01-28 23:50:34 +0100

    Chargement du fichier app_2 et du fichier Readme.md

---
+++ README.md
@@ -0,0 +1,12 @@
+CREATION D'UN JEU VIDEO
+
+L'objectif de ce travail est de mieux comprendre la programmation orientée objets (POO)
+
+Il s'agit de la création d'un jeu vidéo de combat sans graphisme
+
+Les fichiers app.rb, app_2.rb et app_3.rb sont les fichiers à executer.
+
+Il n'y a pas de spec pour cette version comme précisé dans l'énoncé.
+
+BON JEU !!
+
--- app_2.rb
+++ app_2.rb
@@ -4,15 +4,74 @@ Bundler.require
 require_relative 'lib/game'
 require_relative 'lib/player'
 
-player1 = Player.new("José")
+#Affichage d'accueil
+puts "-------------------------------------------------"
+puts "|Bienvenue sur 'ILS VEULENT TOUS MA POO' !      |"
+puts "|Le but du jeu est d'être le dernier survivant !|"
+puts "-------------------------------------------------"
 
-puts player1.name
-puts player1.life_points
+#Demander et enregister le nom du joueur humain
+puts "Entrer votre nom"
+print ">"
+name = gets.chomp
 
-player_human = HumanPlayer.new("Yuness")
+#Definir les objet de Class Player et de Class HumanPlayer
+human_player = HumanPlayer.new(name.to_s)
+player1 = Player.new("Josiane")
+player2 = Player.new("José")
 
-player_human.show_state
-#player_human.search_weapon
-player_human.search_health_pack
-player_human.show_state
-binding.pry
+
+#Mettre les 2 ennemies dans un tableau
+enemies = [player1, player2]
+
+while #condition de sortie quand au moins une Class Player a un attribut @life_points positif
+    human_player.life_points > 0 && (player1.life_points > 0 && player2.life_points > 0)
+
+    human_player.show_state
+    puts ""
+    puts "Qelle action veux-tu effectuer ?"
+    puts "    a - chercher une meilleure arme"
+    puts "    s - chercher à se soigner"
+    puts "Attaquer un joueur en vue :"
+    print "    0 - "
+    player1.show_state
+    print "    1 - "
+    player2.show_state
+
+    puts "Entrer votre choix :"
+    print ">"
+
+    enter = gets.chomp
+
+    #Structure conditionnelle avec case pour gérer 4 cas
+    case enter
+    when "a" 
+            human_player.search_weapon
+    when "s" 
+            human_player.search_health_pack
+    when "0" #A mettre en "" ou à convertir en entier au choix  
+            human_player.attacks(player1)
+    else "1"  
+            human_player.attacks(player2)
+    end
+
+    puts "Les autres joueurs t'attaquent !"
+
+    #Replique des ennemies
+    enemies.each do |enemy|
+        if enemy.life_points > 0
+          enemy.attacks(human_player)
+        end
+      end
+
+end
+
+puts "LA PARTIE EST FINIE"
+
+if human_player.life_points > 0
+    puts "tu as gagné"
+  else
+    puts "tu as perdu"
+end
+
+binding.pry 
